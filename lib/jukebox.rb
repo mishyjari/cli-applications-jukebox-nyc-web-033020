c6cfa6eaad83f65ba4b9f8193fb847e10f2f9376
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-11 03:54:24 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,7 @@ def play(library)
   input = gets.strip
   if library.include?(input)
     puts "Playing #{input}"
-  elsif input <= library.length && input > 0
+  elsif input <= library.length && input.is_a? Integer
     puts "Playing #{library[input]}"
   else
     puts "Invalid Input, please try again"
